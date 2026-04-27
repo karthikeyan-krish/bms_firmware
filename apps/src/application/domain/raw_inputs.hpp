c3f98db37f759001ba97c44c19a4d4a92263ab31
karthikeyan-krish <imkarthiknk@gmail.com>
2026-04-27 19:28:42 +0200

    [DEV] create raw input struct in domain
    Supports both signed and unsigned data

---
+++ apps/src/application/domain/raw_inputs.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+// #include <optional>
+
+namespace bms::domain {
+
+struct TimestampedValueU32 {
+  uint32_t value{0U};
+  uint32_t tick_ms{0U};
+  bool valid{false};
+};
+
+struct TimestampedValueI32 {
+  int32_t value{0};
+  uint32_t tick_ms{0U};
+  bool valid{false};
+};
+
+struct RawInputs {
+  TimestampedValueU32 pack_voltage_mv;
+  TimestampedValueI32 pack_current_ma;
+  TimestampedValueI32 pack_temperature_mc;
+};
+
+}  // namespace bms::domain
