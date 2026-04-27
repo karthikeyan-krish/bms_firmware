ea834f6fea95c63df5d848025b1f1e861f4a6fa4
karthikeyan-krish <imkarthiknk@gmail.com>
2026-04-27 19:29:56 +0200

    [DEV] decouples acquisition thread
    The raw_input_sync decouples the message post
    from depending on any other thread

---
+++ apps/src/application/domain/raw_inputs_sink.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "raw_inputs.hpp"
+
+namespace bms::domain {
+
+class RawInputsSink {
+ public:
+  virtual ~RawInputsSink() = default;
+  virtual void PostRawInputs(const RawInputs& raw_inputs) = 0;
+};
+
+}  // namespace bms::domain
